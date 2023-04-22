1b5689a47ad2a257fa9491a2145a674c771ab815
DeepSweeter <achirica40@gmail.com>
2023-04-22 17:47:25 +0300

    dunno what's going on

---
+++ ConnectionsTests/Conne/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textConsole = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonStartServer = new System.Windows.Forms.Button();
+            this.textBoxChat = new System.Windows.Forms.TextBox();
+            this.textIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // textConsole
+            // 
+            this.textConsole.Location = new System.Drawing.Point(52, 41);
+            this.textConsole.Multiline = true;
+            this.textConsole.Name = "textConsole";
+            this.textConsole.Size = new System.Drawing.Size(682, 226);
+            this.textConsole.TabIndex = 0;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(655, 381);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(95, 36);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(52, 284);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(104, 52);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonStartServer
+            // 
+            this.buttonStartServer.Location = new System.Drawing.Point(52, 373);
+            this.buttonStartServer.Name = "buttonStartServer";
+            this.buttonStartServer.Size = new System.Drawing.Size(104, 52);
+            this.buttonStartServer.TabIndex = 3;
+            this.buttonStartServer.Text = "Start Server";
+            this.buttonStartServer.UseVisualStyleBackColor = true;
+            this.buttonStartServer.Click += new System.EventHandler(this.buttonStartServer_Click);
+            // 
+            // textBoxChat
+            // 
+            this.textBoxChat.Location = new System.Drawing.Point(318, 388);
+            this.textBoxChat.Name = "textBoxChat";
+            this.textBoxChat.Size = new System.Drawing.Size(310, 22);
+            this.textBoxChat.TabIndex = 4;
+            // 
+            // textIP
+            // 
+            this.textIP.Location = new System.Drawing.Point(318, 299);
+            this.textIP.Name = "textIP";
+            this.textIP.Size = new System.Drawing.Size(174, 22);
+            this.textIP.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(279, 302);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "IP";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textIP);
+            this.Controls.Add(this.textBoxChat);
+            this.Controls.Add(this.buttonStartServer);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textConsole);
+            this.Name = "Form1";
+            this.Text = "Chat app";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private  System.Windows.Forms.TextBox textConsole;
+        private  System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonStartServer;
+        private  System.Windows.Forms.TextBox textBoxChat;
+        private System.Windows.Forms.TextBox textIP;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ ConnectionsTests/Conne/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient _client;
+        private Task<TcpClient> _server;
+        public StreamReader _sr;
+        public StreamWriter _sw;
+        public string _receive;
+        public string _textSend;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+
+            foreach(IPAddress ip in localIP)
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    textConsole.ReadOnly = true;
+                    textConsole.AppendText("Your ip " + ip.ToString() + Environment.NewLine);
+                    textConsole.AppendText("Port: 5005" + Environment.NewLine);
+                    textIP.Text = ip.ToString();
+                }
+            }
+        }
+
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            //echivalent cu daca utilizatorul este jucator sau cel care detine camera
+            _client = new TcpClient();
+            IPEndPoint IpEnd = new IPEndPoint(IPAddress.Parse(textIP.Text), 5005);
+
+            try
+            {
+                _client.Connect(IpEnd);
+                textConsole.AppendText("Connected to the server" + Environment.NewLine);
+                _sr = new StreamReader(_client.GetStream());
+                _sw = new StreamWriter(_client.GetStream());
+                _sw.AutoFlush = true;
+                backgroundWorker1.RunWorkerAsync();
+                backgroundWorker2.WorkerSupportsCancellation = true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+          
+        }
+
+        private void buttonStartServer_Click(object sender, EventArgs e)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 5005);
+            listener.Start();
+            _server = listener.AcceptTcpClientAsync();
+            _client = _server.Result;
+            //listener.AcceptTcpClientAsync();
+            //_sr = new StreamReader(_client.GetStream());
+            //_sw = new StreamWriter(_client.GetStream());
+            _sr = new StreamReader(_client.GetStream());
+            _sw = new StreamWriter(_client.GetStream());
+            _sw.AutoFlush = true;
+            backgroundWorker1.RunWorkerAsync();
+            backgroundWorker2.WorkerSupportsCancellation = true;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if(textBoxChat.Text != "")
+            {
+                _textSend = textBoxChat.Text;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            textBoxChat.Text = "";
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (_client.Connected)
+            {
+                try
+                {
+                    _receive = _sr.ReadLine();
+                    this.textConsole.Invoke(new MethodInvoker(delegate ()
+                    {
+                        textConsole.AppendText("Client: " + _receive + Environment.NewLine);
+                    }));
+                    _receive = "";
+
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if(_client.Connected)
+            {
+                _sw.WriteLine(_textSend);
+                this.textConsole.Invoke(new MethodInvoker(delegate ()
+                {
+                    textConsole.AppendText("Me: " + _textSend + Environment.NewLine);
+                }));
+            }
+            else
+            {
+                MessageBox.Show("Sending Failed");
+            }
+            backgroundWorker2.CancelAsync();
+        }
+    }
+}
